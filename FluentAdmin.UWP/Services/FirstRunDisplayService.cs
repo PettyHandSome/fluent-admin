e5d139f7259c8dd4450ddc9e46cbe0e224f3a9f9
Vultur <wall-e@live.cn>
2017-07-29 22:30:33 +0800

    更新通用应用

---
+++ FluentAdmin.UWP/Services/FirstRunDisplayService.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Threading.Tasks;
+
+using FluentAdmin.UWP.Helpers;
+using FluentAdmin.UWP.Views;
+
+using Windows.ApplicationModel;
+
+namespace FluentAdmin.UWP.Services
+{
+    public class FirstRunDisplayService
+    {
+        internal static async Task ShowIfAppropriateAsync()
+        {
+            bool hasShownFirstRun = false;
+            hasShownFirstRun = await Windows.Storage.ApplicationData.Current.LocalSettings.ReadAsync<bool>(nameof(hasShownFirstRun));
+
+            if (!hasShownFirstRun)
+            {
+                await Windows.Storage.ApplicationData.Current.LocalSettings.SaveAsync(nameof(hasShownFirstRun), true);
+                var dialog = new FirstRunDialog();
+                await dialog.ShowAsync();
+            }
+        }
+    }
+}
---
+++ FluentAdmin.UWP/Views/ChartPage.xaml.cs
@@ -0,0 +1,45 @@
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+using FluentAdmin.UWP.Models;
+using FluentAdmin.UWP.Services;
+
+using Windows.UI.Xaml.Controls;
+
+namespace FluentAdmin.UWP.Views
+{
+    public sealed partial class ChartPage : Page, INotifyPropertyChanged
+    {
+        // TODO: UWPTemplates: Change the chart as appropriate to your app.
+        // For help see http://docs.telerik.com/windows-universal/controls/radchart/getting-started
+        public ChartPage()
+        {
+            InitializeComponent();
+        }
+
+        public ObservableCollection<DataPoint> Source
+        {
+            get
+            {
+                // TODO WTS: Replace this with your actual data
+                return SampleDataService.GetChartSampleData();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void Set<T>(ref T storage, T value, [CallerMemberName]string propertyName = null)
+        {
+            if (Equals(storage, value))
+            {
+                return;
+            }
+
+            storage = value;
+            OnPropertyChanged(propertyName);
+        }
+
+        private void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
